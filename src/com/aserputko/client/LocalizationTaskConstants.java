2e98ee08d0f263fee1c8ac7c7294ebd4b3886ab6
Andrew Serputko <aserputko@gmail.com>
2013-09-06 13:02:49 +0300

    first commit

---
+++ src/com/aserputko/client/LocalizationTaskConstants.java
@@ -0,0 +1,10 @@
+package com.aserputko.client;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface LocalizationTaskConstants extends Constants {
+
+	@DefaultStringValue("en_US")
+	String locale();
+		
+}
